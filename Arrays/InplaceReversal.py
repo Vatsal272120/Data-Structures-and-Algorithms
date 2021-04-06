b454aa0458d4d8d22635c058d531e3087c3c23e0
Vatsal <vatsalpatel1073@gmail.com>
2021-04-06 11:28:15 +0530

    Array in place reversal

---
+++ Arrays/InplaceReversal.py
@@ -0,0 +1,17 @@
+
+def reverse(arr):
+
+    start = 0
+    end = len(arr)-1
+
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1 
+        end -= 1
+
+    return arr
+
+
+
+s = reverse([1,2,3,4,5])
+print (s)
