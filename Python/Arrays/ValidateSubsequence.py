bce74b51c4ecb6d2b261af51749faec02dcc3743
vatsal <vatsalpatel1073@gmail.com>
2021-08-16 11:06:51 +0530

    isValidSubsequence.py

--- Python/Arrays/ValidateSubsequence.py
+++ Python/Arrays/ValidateSubsequence.py
@@ -1,8 +1,9 @@
+""" Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not). """
+
 
 """  O(n) Time | O(1) space """
-
-
-
 def isValidSubsequenceOne(array, sequence) :
     seqIdx = 0
     arrayIdx = 0
@@ -16,8 +17,9 @@ def isValidSubsequenceOne(array, sequence) :
 
     return seqIdx == len(sequence)
 
-"""  O(n) Time | O(1) space """
+    
 
+"""  O(n) Time | O(1) space """
 def isValidSubsequenceTwo(array, sequence) :
     seqIdx = 0
 
